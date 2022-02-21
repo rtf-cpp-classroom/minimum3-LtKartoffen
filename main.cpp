f70e79042e2ea95aba218c1dc40d8ce2f48125e8
Krasniuk Yurii <99223621+LtKartoffen@users.noreply.github.com>
2022-02-21 15:09:32 +0200

    added interactive-test for function

--- main.cpp
+++ main.cpp
@@ -1,11 +1,16 @@
 #include "functions.h"
-
+#include <iostream>
 
 int main()
 {
 	//TODO: if you want to test your functions, write your code here
-	
-	
-	
+	using std::cout;
+	cout << "Greetings, user! Enter 3 real-numers and I'm will find a smallest among them:\n";
+	float a, b, c;
+	std::cin >> a >> b >> c;
+	cout << "Okay, and this number...\t"
+		<< findMinValue(a, b, c) << std::endl;
+	cout << "Thank for using. Bye!\n";
+	system("pause");
 	return 0;
 }
